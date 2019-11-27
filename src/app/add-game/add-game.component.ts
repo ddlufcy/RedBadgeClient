962769a3484530c76a6bb4f35760d9489434279f
alekh <alekhawkins17@gmail.com>
2019-11-27 09:45:08 -0500

    k

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Games } from '../models/games';
 import { DatabaseService } from '../services/database.service';
 import { Router } from '@angular/router';
- 
+
 @Component({
   selector: 'app-add-game',
   templateUrl: './add-game.component.html',
@@ -13,23 +13,22 @@ export class AddGameComponent implements OnInit {
 
   id: number;
   data: Games;
- 
+
   constructor(
     public databaseService: DatabaseService,
     public router: Router
   ) {
     this.data = new Games();
   }
- 
+
   ngOnInit() {
   }
- 
+
   submitForm() {
     this.databaseService.createGames(this.data).subscribe((response) => {
       this.router.navigate(['list']);
     });
- 
+
   }
- 
+
 }
- 
