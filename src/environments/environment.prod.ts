3b84bfb1eac57f60e1948b8d1fafdbda6f4b04f3
ddlufcy <ddlufcy@gmail.com>
2019-12-04 14:35:09 -0500

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export let APIURL = '';
 
 switch (window.location.hostname) {
     // this is the deployed angular application
-    case 'https://redbadge.herokuapp.com' :
+    case 'redbadge.herokuapp.com' :
         // this is the full url of your deployed API
         APIURL = 'https://redbadgeserver.herokuapp.com'
         break;
