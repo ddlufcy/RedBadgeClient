e955cb3c82ca11c8901f67a9b6787a11cbf701e9
ddlufcy <ddlufcy@gmail.com>
2019-12-04 12:00:02 -0500

    more changes for deployment

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
-
 import { User } from '../models/user';
+import { APIURL } from '../../environments/environment.prod';
+
 const apiURL = 'http://localhost:3000';
 @Injectable({ providedIn: 'root' })
 export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>(`${apiURL}/auth`);
+        return this.http.get<User[]>(`${APIURL}/auth`);
     }
 
     register(user: User) {
-        return this.http.post(`${apiURL}/auth/signup`, user);
+        return this.http.post(`${APIURL}/auth/signup`, user);
     }
 
     delete(id: number) {
-        return this.http.delete(`${apiURL}/auth/${id}`);
+        return this.http.delete(`${APIURL}/auth/${id}`);
     }
 }
